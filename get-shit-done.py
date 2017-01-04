184e6f2d5f9a0c82aceb40398ff148cf891f6943
gise88 <gianluca.iselli@gmail.com>
2017-01-04 12:33:37 +0100

    restartNetworkingCommand to 'systemctl restart systemd-networkd' - left in the siteList only what I need

--- get-shit-done.py
+++ get-shit-done.py
@@ -1,82 +1,89 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
 import getpass
 import subprocess
 import os
 import json
 
+
 def exit_error(error):
-    print(error)
+    print(error, file=sys.stderr)
     exit(1)
 
+
 iniFile = os.path.expanduser(os.path.join("~", ".get-shit-done.ini"))
-restartNetworkingCommand = ["/etc/init.d/networking", "restart"]
+restartNetworkingCommand = ['systemctl', 'restart', 'systemd-networkd']
 hostsFile = '/etc/hosts'
 startToken = '## start-gsd'
 endToken = '## end-gsd'
-siteList = ['reddit.com', 'forums.somethingawful.com', 'somethingawful.com',
-            'digg.com', 'break.com', 'news.ycombinator.com', 'infoq.com',
-            'bebo.com', 'twitter.com', '', 'blip.com',
-            'youtube.com', 'vimeo.com', 'delicious.com', 'flickr.com',
-            'friendster.com', 'hi5.com', 'linkedin.com', 'livejournal.com',
-            'meetup.com', 'myspace.com', 'plurk.com', 'stickam.com',
-            'stumbleupon.com', 'yelp.com', 'slashdot.com','thedailywtf.com',
-        'facebook.com','okcupid.com','craigslist.org','hasgeek.com',
-            'questionablecontent.net', 'xkcd.com', 'smbc-comics.com','oglaf.com',
-            'limbero.org','existentialcomics.com', 'zenpencils.com','phdcomics.com',
-            'lesswrong.com','plus.google.com', 'moneycontrol.com', 'quora.com'
-            ]
-
+siteList = [
+    '9gag.com',
+    '9gag.tv',
+    'facebook.com',
+    'instagram.com',
+    'reddit.com',
+    'slashdot.com',
+    'smbc-comics.com',
+    'thedailywtf.com',
+    'xkcd.com',
+    'oglaf.com',
+]
 
 if os.path.exists(iniFile):
-    iniF = open(iniFile)
+    ini_file = open(iniFile)
     try:
-        iniF_in = iniF.read()
-        print(iniF_in)
-        iniF_out = json.loads(iniF_in)
-        if iniF_out.has_key ("sites"):
-            siteList = siteList + iniF_out.get("sites")
-        elif iniF_out.has_key ("siteList"):
-            siteList =  iniF_out.get("siteList")
+        ini_content = ini_file.read()
+        print(ini_content)
+        ini_json = json.loads(ini_content)
+        if "sites" in ini_json:
+            siteList = siteList + ini_json.get("sites")
+        elif "siteList" in ini_json:
+            siteList = ini_json.get("siteList")
     finally:
-        iniF.close()
+        ini_file.close()
+
 
 def rehash():
     subprocess.check_call(restartNetworkingCommand)
 
+
 def work():
-    hFile = open(hostsFile, 'a+')
-    contents = hFile.read()
+    h_file = open(hostsFile, 'a+')
+    contents = h_file.read()
     if startToken in contents and endToken in contents:
         exit_error("Work mode already set.")
-        hFile.close()
+        h_file.close()
     else:
-        hFile.write(startToken + "\n")
+        h_file.write(startToken + "\n")
         for site in siteList:
-            hFile.write("127.0.0.10\t" + site + "\n")
-            hFile.write("127.0.0.10\twww." + site + "\n")
-        hFile.write(endToken)
-    hFile.close()
+            h_file.write("0.0.0.0\t" + site + "\n")
+            h_file.write("0.0.0.0\twww." + site + "\n")
+        h_file.write(endToken)
+    h_file.close()
     rehash()
 
+
 def play():
-    hFile = open(hostsFile, "r+")
-    lines = hFile.readlines()
-
-    startIndex = -1
-
+    h_file = open(hostsFile, "r+")
+    lines = h_file.readlines()
+    
+    start_index = -1
+    
     for index, line in enumerate(lines):
         if line.strip() == startToken:
-            startIndex = index
-    if startIndex > -1:
-        lines = lines[0:startIndex]
-
-        hFile.seek(0)
-        hFile.write(''.join(lines))
-        hFile.truncate()
-
+            start_index = index
+    if start_index > -1:
+        lines = lines[0:start_index]
+        
+        h_file.seek(0)
+        h_file.write(''.join(lines))
+        h_file.truncate()
+        
         rehash()
+
 
 def main():
     if getpass.getuser() != 'root':
@@ -84,9 +91,9 @@ def main():
     if len(sys.argv) != 2:
         exit_error('usage: ' + sys.argv[0] + ' [work|play]')
     try:
-        {"work": work, "play": play}[sys.argv[1]]()
+        { "work": work, "play": play }[sys.argv[1]]()
     except Exception as e:
-        print(e)
+        exit_error(e)
 
 
 if __name__ == '__main__':
